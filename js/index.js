a79dc12227a4caf6f2787efa8a0e1fe04096dd54
Abraham Bisrat <abrahammehari0@gmail.com>
2022-03-21 04:23:48 -0500

    Added Search bar with impl

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
 const p = (text) => console.log(text);
 const blogContainer = document.querySelector(".blogs");
+const searchBar = document.forms[0];
 
-const posts = async () => {
+searchBar.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(searchBar.term.value);
+  renderPosts(searchBar.term.value);
+});
+
+const posts = async (query) => {
   let uri = "http://localhost:3000/posts?_sort=likes&_order=desc";
+  if (query) {
+    uri += `&q=${query}`;
+  }
   return await (await fetch(uri)).json();
 };
 const users = async () => {
   const uri = "http://localhost:3000/users";
   return await (await fetch(uri)).json();
 };
-function renderPosts() {
-  posts()
+function renderPosts(query) {
+  posts(query)
     .then((blogs) => {
       let template = "";
       blogs.forEach((blog) => {
